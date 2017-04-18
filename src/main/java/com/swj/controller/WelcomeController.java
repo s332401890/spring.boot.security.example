0ec6f774f45e5bbf8e63efc57853e571b9544cc9
swj <332401890@qq.com>
2017-04-18 21:11:51 +0800

    add jsp

--- src/main/java/com/swj/controller/WelcomeController.java
+++ src/main/java/com/swj/controller/WelcomeController.java
@@ -1,15 +1,23 @@
 package com.swj.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController {
-	
+
 	@RequestMapping("/")
 	@ResponseBody
-	public String helloWorld(){
+	public String helloWorld() {
 		return "hello world!";
+	}
+
+	@RequestMapping("/welcome")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", "hello world!");
+		return "welcome";
 	}
 }
